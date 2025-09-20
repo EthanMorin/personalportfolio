bcda789d988a68e06f865e54403208a496cc5aca
EthanMorin <EthanMorin2004@gmail.com>
2025-09-19 18:47:50 -0600

    fixed sqlitie import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  nitro: { preset: 'cloudflare_pages' }, // good practice on Pages
+  content: {
+    experimental: { sqliteConnector: 'native' }, // << key line
+  },
+
   modules: [
     '@nuxt/ui',
     '@nuxt/test-utils',
