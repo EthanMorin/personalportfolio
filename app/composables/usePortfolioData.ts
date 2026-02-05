ac17068ddff314f0950ad2a1cda750e30d340b2e
EthanMorin <EthanMorin2004@gmail.com>
2026-02-04 21:33:43 -0700

    Update usePortfolioData.ts

--- app/composables/usePortfolioData.ts
+++ app/composables/usePortfolioData.ts
@@ -53,7 +53,7 @@ export const usePortfolioData = () => {
         "A responsive demo store built with React, TypeScript, and Tailwind CSS. It features smooth product browsing, an interactive cart with real-time updates, and a mock checkout flow that highlights secure form handling and fast performance across devices—all in a production-style design with no real purchases.",
       technologies: ["React", "Tailwind CSS", "TypeScript"],
       image:
-        "https://cdn.discordapp.com/attachments/1419238894381039648/1419254459279609948/image.png?ex=68d11737&is=68cfc5b7&hm=7888f8ef9ee71f85e34b7a1ac030d561d633c48531a2122e11c6cb62d7e8e38c&",
+        "",
       github: "https://github.com/EthanMorin/shopease",
       live: "https://shopease.ethanmorin.dev/",
     },
@@ -63,7 +63,7 @@ export const usePortfolioData = () => {
         "Modern kanban board with React 18, TypeScript, and Zustand state management. Features drag-and-drop card management, inline editing, multiple color themes, and glassmorphism UI design. Built with Vite for fast development and includes dark/light mode support with accessible components.",
       technologies: ["React", "TypeScript", "Tailwind CSS", "Zustand"],
       image:
-        "https://cdn.discordapp.com/attachments/1419238894381039648/1419238912525598720/image.png?ex=68d108bc&is=68cfb73c&hm=f8860f6c6a805dc60751825bd9bdd2c6b50b2d1bb81dd92fa63a275b7eaba176&",
+        "",
       github: "https://github.com/EthanMorin/Atlas-Todo",
       live: "https://atlas-todo.ethanmorin.dev/",
     },
